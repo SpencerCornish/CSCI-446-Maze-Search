2176e66a105e6bcf1ca4c06c5f434b318631b40c
Bryan Plant <bryanplant@gmail.com>
2018-10-01 14:48:15 -0600

    add width and height to maze model

--- maze.py
+++ maze.py
@@ -2,6 +2,8 @@ class Maze:
     data = []
     start = ()
     end = ()
+    width = 0
+    height = 0
 
     def __init__(self, maze_file):
         with open(maze_file, 'r') as file:
@@ -12,3 +14,5 @@ class Maze:
                     self.start = (x, line_list.index('P'))
                 if '*' in line:
                     self.end = (x, line_list.index('*'))
+        self.width = len(self.data[0])
+        self.height = len(self.data)
