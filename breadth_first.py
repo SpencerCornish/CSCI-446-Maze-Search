54d073f6c408283b50a7f722aace4bc02ab1234e
Keely Weisbeck <keelyweisbeck@gmail.com>
2018-10-03 12:56:08 -0600

    updates to breadth_first_search and depth_first_search

--- breadth_first.py
+++ breadth_first.py
@@ -1,7 +1,26 @@
 from algorithm import Algorithm
+from collections import deque
 
 
 class BreadthFirst(Algorithm):
-
     def search(self, maze):
-        pass
+        start = maze.start
+        end = maze.end
+        queue = deque([start])
+        explored = []
+
+        while queue:
+            cur = queue.popleft()
+            self.num_steps += 1
+
+            if cur not in explored:
+                explored.append(cur)
+                self.num_expanded += 1
+
+                neighbors = self.get_neighbors(maze, cur)
+                for n in neighbors:
+                    if n not in explored:
+                        if n != end:
+                            queue.append(n)
+                        else:
+                            print("found end")
