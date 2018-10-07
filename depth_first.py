36b8aaae3846a2105da95f27d3209523f4c2abe5
Bryan Plant <bryanplant@gmail.com>
2018-10-07 10:41:13 -0600

    update depth_first to use len(stack) > 0 for while loop, better comments

--- depth_first.py
+++ depth_first.py
@@ -4,27 +4,26 @@ from algorithm import Algorithm
 class DepthFirst(Algorithm):
 
     def search(self, maze):
+        stack = []  # the stack
         current_node = maze.start
         self.visited.append([current_node.x, current_node.y])
-        q = []  # the stack
-        done = False
+        stack.append(current_node)
 
-        while not done:
+        while len(stack) > 0:
             # get all valid neighbors around current position
             neighbors = self.get_neighbors(maze, current_node)
             for node in neighbors:
-                # add neighbors to heap in format (distance_to_end, neighbor_node)
-                q.append(node)
+                # add neighbors to stack
+                stack.append(node)
                 self.visited.append([node.x, node.y])
                 # check for end
                 if maze.data[node.x][node.y] == '*':
-                    current_node = node
-                    print("Found finish!")
-                    done = True
-                    break
-            if not done:
-                # set current node to closest node to end in heap
-                current_node = q.pop()
+                    self.update_path(node)
+                    maze.draw(self.visited, self.path)
+                    return
+
+            # set current node to next node in stack
+            current_node = stack.pop()
             self.update_path(current_node)
             maze.draw(self.visited, self.path)
 
