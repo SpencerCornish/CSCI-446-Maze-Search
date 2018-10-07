283aeb599264a9c929a691a1b58969887aa7db1a
Bryan Plant <bryanplant@gmail.com>
2018-10-07 10:36:49 -0600

    update greedy to use len(q) > 0 for while loop

--- greedy.py
+++ greedy.py
@@ -8,9 +8,9 @@ class Greedy(Algorithm):
         q = []  # the heap
         current_node = maze.start
         self.visited.append([current_node.x, current_node.y])
-        done = False
+        heapq.heappush(q, (self.get_distance(current_node, maze.end), current_node))
 
-        while not done:
+        while len(q) > 0:
             # get all valid neighbors around current position
             neighbors = self.get_neighbors(maze, current_node)
             for node in neighbors:
@@ -19,12 +19,12 @@ class Greedy(Algorithm):
                 self.visited.append([node.x, node.y])
                 # check for end
                 if maze.data[node.x][node.y] == '*':
-                    current_node = node
-                    print("Found finish!")
-                    done = True
-                    break
-            if not done:
-                # set current node to closest node to end in heap
-                current_node = heapq.heappop(q)[1]
+                    self.update_path(node)
+                    maze.draw(self.visited, self.path)
+                    return
+
+            # set current node to closest node to end in heap
+            current_node = heapq.heappop(q)[1]
+            # update path to current_node and draw
             self.update_path(current_node)
             maze.draw(self.visited, self.path)
