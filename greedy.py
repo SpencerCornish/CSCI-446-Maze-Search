cb353d8a25375194147b27e774aabf9694a1dd3c
Bryan Plant <bryanplant@gmail.com>
2018-10-07 11:42:26 -0600

    check for greedy goal state when node is popped

--- greedy.py
+++ greedy.py
@@ -7,7 +7,6 @@ class Greedy(Algorithm):
     def search(self, maze):
         q = []  # the heap
         current_node = maze.start
-        self.visited.append([current_node.x, current_node.y])
         heapq.heappush(q, (self.get_distance(current_node, maze.end), current_node))
 
         while len(q) > 0:
@@ -17,14 +16,15 @@ class Greedy(Algorithm):
             self.update_path(current_node)
             maze.draw(self.visited, self.path)
 
+            # check for end
+            if maze.data[current_node.x][current_node.y] == '*':
+                return
+
             # get all valid neighbors around current position
             neighbors = self.get_neighbors(maze, current_node)
             for node in neighbors:
                 # add neighbors to heap in format (distance_to_end, neighbor_node)
-                heapq.heappush(q, (self.get_distance(node, maze.end), node))
-                self.visited.append([node.x, node.y])
-                # check for end
-                if maze.data[node.x][node.y] == '*':
-                    self.update_path(node)
-                    maze.draw(self.visited, self.path)
-                    return
+                if (self.get_distance(node, maze.end), node) not in q:
+                    heapq.heappush(q, (self.get_distance(node, maze.end), node))
+
+            self.visited.append([current_node.x, current_node.y])
